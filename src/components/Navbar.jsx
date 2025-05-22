98ee32493bd4a78d738750455281b12acfe39a06
Dhananjay Sable <125184802+dhananjaysable370@users.noreply.github.com>
2025-05-22 14:19:15 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -158,7 +158,7 @@ const Navbar = () => {
               to="responsive-navbar-iceico-task/"
               className="flex items-center"
             >
-              <span className="mr-2">🚀</span>
+              <span className="mr-2 sm:text-xl">🚀</span>
               TechSolutions
             </Link>
           </motion.div>
